d3e2b71d0370d183e11985685950cc28738b82d4
Steve Chao <22403555+stchao@users.noreply.github.com>
2023-02-13 20:54:10 -0500

    Update erase to toggle and add title to buttons

--- script.js
+++ script.js
@@ -95,10 +95,12 @@ function redrawContainer(sideLength) {
 
 function etchBackground(ev) {    
     let randomButton = document.querySelector("#randomButton");
-    let colorWheel = document.querySelector("#colorWheel");        
+    let colorWheel = document.querySelector("#colorWheel");
+    let eraseButton = document.querySelector("#eraseButton");    
     let isMouseClicked = ev.buttons === 1 || ev.type === "mousedown";
     let isRandomEnabled = randomButton.classList.contains("toggle-enabled");
-    let color = !isRandomEnabled ? colorWheel.value : getRandomColor();
+    let isEraseEnabled = eraseButton.classList.contains("toggle-enabled");  
+    let color = !isRandomEnabled || isEraseEnabled ? colorWheel.value : getRandomColor();
     let isSketchElementAlreadyThisColor = ev.target.style.backgroundColor === color;
     
     if (!isMouseClicked || isSketchElementAlreadyThisColor) {
@@ -134,24 +136,21 @@ function removeSketchElements(sideLength) {
 }
 
 function toggleErase() {
-    let eraseIcon = document.querySelector("#eraseIcon");
-    let pencilIcon = document.querySelector("#pencilIcon");
+    let eraseButton = document.querySelector("#eraseButton");
     let colorWheel = document.querySelector("#colorWheel");
-    let isEraseIconHidden = eraseIcon.classList.contains("display-none");
+    let isEraseEnabled = eraseButton.classList.contains("toggle-enabled");
 
-    if (!isEraseIconHidden) {
-        pencilIcon.classList.toggle("display-none");
-        eraseIcon.classList.toggle("display-none");
+    eraseButton.classList.toggle("toggle-enabled");
+
+    if (!isEraseEnabled) {        
         colorWheel.setAttribute("data-previousColor", colorWheel.value);
         updateSketchElementsColor(defaultEraseColor);        
         return;
     }
     
     let color = colorWheel.dataset.previouscolor || defaultColor;
-    updateSketchElementsColor(color);
-    pencilIcon.classList.toggle("display-none");
-    eraseIcon.classList.toggle("display-none");
     colorWheel.setAttribute("data-previousColor", "");
+    updateSketchElementsColor(color);    
 }
 
 function printSketch() {
